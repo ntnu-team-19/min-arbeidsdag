3cd1795b9a5940a4535bbde73aee120acde12666
johanarntsen <johamar@stud.ntnu.no>
2026-03-10 14:51:27 +0100

    feat: make routing to dashboard as landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { DashboardPage } from './features/dashboard/pages/dashboard-page/dashboard-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: DashboardPage },
+];
