8288947092f0599f16efb32724cc6f2dd13d6dd1
johanarntsen <johamar@stud.ntnu.no>
2026-03-11 08:54:17 +0100

    refactor: run prettier

--- src/app/features/dashboard/components/day-selector/day-selector.ts
+++ src/app/features/dashboard/components/day-selector/day-selector.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { NgClass} from '@angular/common';
+import { NgClass } from '@angular/common';
 import { DayOption } from './day-selector.types';
 
 interface SelectorItem {
