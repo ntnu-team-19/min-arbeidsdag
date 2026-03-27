06817e21fe54e5d05612b5bff14edd6ad00735db
Anders <alunde@stud.ntnu.no>
2026-03-27 10:39:38 +0100

    feature: Update providers and add translate service and loader

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,10 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 import { providePrimeNG } from 'primeng/config';
 import Lara from '@primeng/themes/lara';
+import { provideTranslateService } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { routes } from './app.routes';
 
@@ -9,10 +12,13 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
+    provideHttpClient(),
     providePrimeNG({
       theme: {
         preset: Lara,
       },
     }),
+    provideTranslateService({ fallbackLang: 'no' }),
+    ...provideTranslateHttpLoader({ prefix: '/assets/i18n/', suffix: '.json' }),
   ],
 };
