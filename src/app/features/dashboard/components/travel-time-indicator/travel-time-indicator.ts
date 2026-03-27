474d915715deb1f0be08e32902c749a83fdd1dab
Anders <alunde@stud.ntnu.no>
2026-03-27 10:36:57 +0100

    feature: Translation added to travel-time-indicator

--- src/app/features/dashboard/components/travel-time-indicator/travel-time-indicator.ts
+++ src/app/features/dashboard/components/travel-time-indicator/travel-time-indicator.ts
@@ -1,8 +1,10 @@
 import { Component, Input } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-travel-time-indicator',
   standalone: true,
+  imports: [TranslatePipe],
   templateUrl: './travel-time-indicator.html',
   styleUrl: './travel-time-indicator.css',
 })
