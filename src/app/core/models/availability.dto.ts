722d1efa8e343289314e0bf3c28faec52bb0909b
Anders <alunde@stud.ntnu.no>
2026-04-10 15:38:17 +0200

    feature: added AvailabilityDto model

---
+++ src/app/core/models/availability.dto.ts
@@ -0,0 +1,13 @@
+import { AssignmentLocationPoint } from './assignment-card.model';
+
+export interface AvailabilityDto {
+  start: string;
+  stop: string;
+  calculatedTraveltime: number;
+  available: boolean;
+  allDay: boolean;
+  absenceWithoutGoingHome: boolean;
+  locationPoint: AssignmentLocationPoint | null;
+}
+
+
